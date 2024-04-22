bbe0915d680ea75179141b9cf52670d17101662c
robin <robin.e.unger@gmail.com>
2024-04-22 17:39:55 -0500

    Create getComputerChoice
    - Create function that returns "rock", "paper" or "scissors"

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const CHOICES = ["rock", "paper", "scissors"];
+
+// Get a random int up to a max int (starting at 0)
+function getRandomInt(maxInt) {
+  return Math.floor(Math.random() * maxInt);
+}
 // Ask player for their choice of rock, paper or scissors.
 // Capture player choice in playerChoice variable
 // Calculate computer choice
+function getComputerChoice() {
+  return CHOICES[getRandomInt(CHOICES.length)];
+}
+  
 // Capture computer choice in computerChoice variable
 // Compare playerChoice to computerChoice
 // If playerChoice is "rock" and computerChoice is "scissors" player wins
