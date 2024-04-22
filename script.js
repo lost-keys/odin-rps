7c3bc2b971adc2f74601ed1f2e6a0fda012fd039
robin <robin.e.unger@gmail.com>
2024-04-22 17:11:09 -0500

    Add initial template files

---
+++ script.js
@@ -0,0 +1,12 @@
+// Ask player for their choice of rock, paper or scissors.
+// Capture player choice in playerChoice variable
+// Calculate computer choice
+// Capture computer choice in computerChoice variable
+// Compare playerChoice to computerChoice
+// If playerChoice is "rock" and computerChoice is "scissors" player wins
+// If playerChoice is "paper" and computerChoice is "rock" player wins
+// If playerChoice is "scissors" and computerChoice is "paper" player wins
+// If playerChoice is "rock" and computerChoice is "paper" player loses
+// If playerChoice is "paper" and computerChoice is "scissors" player loses
+// If playerChoice is "scissors" and computerChoice is "rock" player loses
+// Else, game is a draw
